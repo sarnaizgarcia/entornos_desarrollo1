113d277cc063df957d0cbb4c6a028a03cb437513
Silvia <sarnaizgarcia@gmail.com>
2021-01-23 10:12:24 +0100

    Elimino codigo de clase main

--- Calculadora_Act1/src/calculadora/Calculadora.java
+++ Calculadora_Act1/src/calculadora/Calculadora.java
@@ -12,18 +12,6 @@ public class Calculadora {
 
 	public static void main(String[] args) {
 		
-		Cociente c = new Cociente();
-		
-		float divReales = c.cocienteReales(10.5F, 3.5F);
-		int divEnteros = c.cocienteEnteros(10, 5);
-		float inv = c.inverso(10F);
-		double raizCuadrada = c.raiz(16);
-		
-		System.out.println(divReales);
-		System.out.println(divEnteros);
-		System.out.println(inv);
-		System.out.println(raizCuadrada);
-
 	}
 
 }
